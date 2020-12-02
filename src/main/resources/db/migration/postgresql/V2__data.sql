49378d3a6a0fb24361b621319f53d39f337e6308
James Land <hokie10@gmail.com>
2020-12-02 13:16:01 -0500

    V2 fixes

--- src/main/resources/db/migration/postgresql/V2__data.sql
+++ src/main/resources/db/migration/postgresql/V2__data.sql
@@ -1,1 +1,1 @@
-INSERT INTO todo_v2 (name, description, date, completed) values ('Split The Service', 'Prove that we can split the backend service in 2', (NOW() - INTERVAL '2 DAY'), true)
+INSERT INTO todov2 (name, description, date, completed) values ('Split The Service', 'Prove that we can split the backend service in 2', (NOW() - INTERVAL '2 DAY'), true)
