59725d0cc9d1098f23cca9c83088d0ce48c0205c
James Land <hokie10@gmail.com>
2020-12-02 13:50:15 -0500

    V2 fixes

--- src/main/resources/db/migration/h2/V2__data.sql
+++ src/main/resources/db/migration/h2/V2__data.sql
@@ -1,2 +1,2 @@
-INSERT INTO todo_v2 (name, description, date, completed) VALUES
+INSERT INTO todov2 (name, description, date, completed) VALUES
   ('Split The Service', 'Prove that we can split the backend service in 2', now() - INTERVAL 2 DAY, true)
--- src/main/resources/db/migration/postgresql/V1__ddl.sql
+++ src/main/resources/db/migration/postgresql/V1__ddl.sql
@@ -1,7 +1,8 @@
 CREATE TABLE todov2 (
-  id INT IDENTITY NOT NULL  PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(250) NOT NULL,
   description VARCHAR(250) NOT NULL,
-  date DATETIME NOT NULL,
+  date TIMESTAMP NOT NULL,
   completed Boolean DEFAULT false
 );
+
